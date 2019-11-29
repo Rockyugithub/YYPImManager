e30f64532c7d4eaea83697277f38d16e884e7b99
Apple <>
2019-11-29 16:49:31 +0800

    添加开源许可和podspec

---
+++ Example/Pods/YYPImManager/YYPImManager/Classes/ImManager.swift
@@ -0,0 +1,15 @@
+//
+//  ImManager.swift
+//  YYPImManager_Example
+//
+//  Created by Apple on 2019/11/29.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+public class ImManager: NSObject {
+    public func testFunc() {
+        print("this is a test")
+    }
+}
